9f6e0f98303fcc5fddc429f0753c3b7ea6c11a17
LA <1245661240@qq.com>
2026-01-02 14:54:55 +0800

    [mania]修改落速枚举代码防兼容问题

--- osu.Game.Rulesets.Mania/LAsEZMania/EzManiaEnums.cs
+++ osu.Game.Rulesets.Mania/LAsEZMania/EzManiaEnums.cs
@@ -1,6 +1,7 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
 using System.ComponentModel;
 
 namespace osu.Game.Rulesets.Mania.LAsEZMania
@@ -9,16 +10,22 @@ namespace osu.Game.Rulesets.Mania.LAsEZMania
     {
         // [LocalisableDescription(typeof(RulesetSettingsStrings), nameof(RulesetSettingsStrings.ScrollingDirectionUp))]
         [Description("40速 通配速度风格(不可用)")]
-        ScrollSpeedStyle,
+        ScrollSpeedStyle = 0,
 
         // [LocalisableDescription(typeof(RulesetSettingsStrings), nameof(RulesetSettingsStrings.ScrollingDirectionDown))]
         [Description("ms值 相对默认判定线")]
-        ScrollTimeForDefaultJudgement,
+        ScrollTimeForDefaultJudgement = 1,
 
         [Description("ms值 相对实际判定线")]
-        ScrollTimeForRealJudgement,
+        ScrollTimeForRealJudgement = 2,
 
         [Description("ms值 相对屏幕底部")]
-        ScrollTimeForScreenBottom,
+        ScrollTimeForScreenBottom = 3,
+
+        [Obsolete("Renamed to ScrollTimeForScreenBottom. Kept for backward compatibility with stored settings.")]
+        ScrollTimeStyle = ScrollTimeForScreenBottom,
+
+        [Obsolete("Renamed to ScrollTimeForScreenBottom. Kept for backward compatibility with stored settings.")]
+        ScrollTimeStyleFixed = ScrollTimeForScreenBottom,
     }
 }
