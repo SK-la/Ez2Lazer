f8e324684da2f78c53cfbf02765adedaf2fc9c78
LA <1245661240@qq.com>
2026-02-01 23:14:00 +0800

    改为bms规范的音频轨道

---
+++ osu.Game.Rulesets.BMS/Objects/BmsManiaNote.cs
@@ -0,0 +1,21 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System.Collections.Generic;
+using osu.Game.Audio;
+using osu.Game.Rulesets.Mania.Objects;
+
+namespace osu.Game.Rulesets.BMS.Objects
+{
+    /// <summary>
+    /// Mania note with BMS keysound samples attached.
+    /// </summary>
+    public class BmsManiaNote : Note, IBmsKeysoundProvider
+    {
+        public List<HitSampleInfo> KeysoundSamples { get; init; } = new List<HitSampleInfo>();
+
+        IReadOnlyList<HitSampleInfo> IBmsKeysoundProvider.KeysoundSamples => KeysoundSamples;
+
+        public override IList<HitSampleInfo> AuxiliarySamples => KeysoundSamples;
+    }
+}
