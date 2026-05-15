7462e3b291d92c3b9d7c82de9d1ea344f6f93996
LA <1245661240@qq.com>
2026-05-15 22:43:53 +0800

    修改分数进程，解决借用mania规则集不计算PP问题

--- osu.Game.Rulesets.BMS/Scoring/BMSScoreProcessor.cs
+++ osu.Game.Rulesets.BMS/Scoring/BMSScoreProcessor.cs
@@ -1,6 +1,7 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using osu.Game.Rulesets.Mania.Scoring;
 using osu.Game.Rulesets.Scoring;
 
 namespace osu.Game.Rulesets.BMS.Scoring
@@ -9,17 +10,27 @@ namespace osu.Game.Rulesets.BMS.Scoring
     /// Score processor for BMS ruleset.
     /// Uses EX SCORE style scoring (PGREAT=2, GREAT=1, etc.)
     /// </summary>
-    public partial class BMSScoreProcessor : ScoreProcessor
+    public partial class BMSScoreProcessor : ManiaScoreProcessor
     {
-        public BMSScoreProcessor()
-            : base(new BMSRuleset())
-        {
-        }
-
         protected override double ComputeTotalScore(double comboProgress, double accuracyProgress, double bonusPortion)
         {
             // BMS-style EX SCORE calculation
-            return 500000 * accuracyProgress + 500000 * comboProgress;
+            return 1000000 * accuracyProgress;
+        }
+
+        public override int GetBaseScoreForResult(HitResult result)
+        {
+            switch (result)
+            {
+                case HitResult.Perfect:
+                    return 2;
+
+                case HitResult.Great:
+                    return 1;
+
+                default:
+                    return 0;
+            }
         }
     }
 }
