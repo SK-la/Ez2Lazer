f7dffce27317abd2f780a462741f1d040a09b8d0
LA <1245661240@qq.com>
2026-05-28 00:48:21 +0800

    强制重建只在debug时使用

--- osu.Game/EzOsuGame/Overlays/EzUISettings.cs
+++ osu.Game/EzOsuGame/Overlays/EzUISettings.cs
@@ -4,10 +4,12 @@
 using osu.Framework.Localisation;
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
 using osu.Game.Database;
 using osu.Game.EzOsuGame.Configuration;
 using osu.Game.EzOsuGame.Localization;
 using osu.Game.Graphics.UserInterfaceV2;
+using osu.Game.Overlays;
 using osu.Game.Overlays.Settings;
 
 namespace osu.Game.EzOsuGame.Overlays
@@ -39,19 +41,31 @@ namespace osu.Game.EzOsuGame.Overlays
                 {
                     Keywords = new[] { "analysis", "sqlite", "cache", "warmup", "persistent" }
                 },
-                new SettingsButtonV2
+                new FillFlowContainer
                 {
-                    Text = EzSettingsStrings.EZ_REALM_METADATA_BACKFILL_BUTTON,
-                    TooltipText = EzSettingsStrings.EZ_REALM_METADATA_BACKFILL_TOOLTIP,
-                    Action = () => backgroundDataStoreProcessor.QueueEzRealmMetadataBackfill(),
-                    Keywords = new[] { "realm", "tag", "xxy", "pp", "metadata", "backfill" },
-                },
-                new SettingsButtonV2
-                {
-                    Text = EzSettingsStrings.EZ_REALM_METADATA_BACKFILL_FORCE_BUTTON,
-                    TooltipText = EzSettingsStrings.EZ_REALM_METADATA_BACKFILL_FORCE_TOOLTIP,
-                    Action = () => backgroundDataStoreProcessor.QueueEzRealmMetadataBackfill(forceAll: true),
-                    Keywords = new[] { "realm", "tag", "xxy", "pp", "metadata", "force", "recalculate" },
+                    RelativeSizeAxes = Axes.X,
+                    AutoSizeAxes = Axes.Y,
+                    Direction = FillDirection.Horizontal,
+                    Padding = SettingsPanel.CONTENT_PADDING,
+                    Children = new Drawable[]
+                    {
+                        new SettingsButton
+                        {
+                            Text = EzSettingsStrings.EZ_REALM_METADATA_BACKFILL_BUTTON,
+                            TooltipText = EzSettingsStrings.EZ_REALM_METADATA_BACKFILL_TOOLTIP,
+                            Action = () => backgroundDataStoreProcessor.QueueEzRealmMetadataBackfill(),
+                            Keywords = new[] { "realm", "tag", "xxy", "pp", "metadata", "backfill" },
+                        },
+#if DEBUG
+                        new SettingsButton
+                        {
+                            Text = EzSettingsStrings.EZ_REALM_METADATA_BACKFILL_FORCE_BUTTON,
+                            TooltipText = EzSettingsStrings.EZ_REALM_METADATA_BACKFILL_FORCE_TOOLTIP,
+                            Action = () => backgroundDataStoreProcessor.QueueEzRealmMetadataBackfill(forceAll: true),
+                            Keywords = new[] { "realm", "tag", "xxy", "pp", "metadata", "force", "recalculate" },
+                        },
+# endif
+                    }
                 },
                 new SettingsItemV2(new FormCheckBox
                 {
